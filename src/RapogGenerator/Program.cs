5c0ce83bf1e73d4c092700d83d0b40b8b60b945d
Mathieu Balland <mathieu.balland@dptechnology.com>
2019-05-30 18:08:19 -0700

    Use command line parse

--- src/RapogGenerator/Program.cs
+++ src/RapogGenerator/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using CommandLine;
 
 namespace RapogGenerator
 {
     class Program
     {
+        public class Options
+        {
+            [Option('i', "inputFolder", Required = true, HelpText = "Set the folder with the articles.")]
+            public string InputFolder { get; set; }
+
+            [Option('o', "outputFolder", Default=".\\build\\", HelpText = "Set the output folder.")]
+            public string OutputFolder { get; set; }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Parser.Default.ParseArguments<Options>(args).WithParsed(o =>
+            {
+            });
         }
     }
 }
